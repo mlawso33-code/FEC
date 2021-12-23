194e870f3f914983bdf4835f737be0baf10a85d0
Sterling Muller <sterlingmuller93@gmail.com>
2021-12-23 12:41:23 -0600

    added api token and get handler / request for products in server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const axios = require('axios')
+const token = require('../config.js');
+const { response } = require('express');
 
 const app = express();
 
@@ -8,3 +11,31 @@ app.listen(PORT, () => {console.log(`Listening on port: ${PORT}`)})
 app.use(express.json())
 
 app.use('/', express.static(__dirname + '/../client/dist'))
+
+const api = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-den'
+
+
+//Sterling CRUD
+app.get('/products', (req, res) => {
+  let {page, count} = req.body;
+  const header = {headers: {
+    'Authorization': token.TOKEN
+  }}
+
+  if(!page) {
+    page = 1;
+  }
+  if (!count) {
+    count = 5;
+  }
+
+  axios.get(api + `/products?page=${page}&count=${count}`, header)
+    .then(response => {
+      res.send(response.data) })
+    .catch(err => res.status(400).send("ERROR", err))
+})
+
+//Marc CRUD
+
+
+//Mitchell CRUD
