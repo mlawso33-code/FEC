c4ba66f982786f63ab162fb75723d9d596d7672e
Mitchell <mitchellwgardner@gmail.com>
2021-12-30 12:45:58 -0700

    Building out Rating Summary

--- client/src/components/Ratings&Reviews/RatingBreakdown.jsx
+++ client/src/components/Ratings&Reviews/RatingBreakdown.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react"
+import axios from "axios"
+import RatingsAndReviewsContext from './RatingsandReviewsContext.jsx'
 
 const RatingBreakdown = () => {
+  const { product } = useContext(AppContext)
+  const {product_id} = useContext(RatingsAndReviewsContext)
+  const [ratingSummary, setRatingSummary] = useState([])
+
+  function fetchMetaData() {
+    axios.get(`/api/reviews/meta/?product_id=${product_id}`)
+      .then(response => setRatingSummary(response.data.ratings))
+  }
+
+  useEffect(() => {
+    if(JSON.stringify(product) !== '{}') {
+      fetchMetaData()
+    }
+  }, [product])
 
   return (
     <div>
-      hello
+      {/* Rating Summary */}
+      <div>
+        <span></span>
+        <Rating />
+      </div>
     </div>
   )
 }
