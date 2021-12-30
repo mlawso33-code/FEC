54a80f910f6d1f45827a1124535a3e0062441d12
Mitchell <mitchellwgardner@gmail.com>
2021-12-30 14:44:25 -0700

    Implemented Average Rating in number format

--- client/src/components/Ratings&Reviews/RatingBreakdown.jsx
+++ client/src/components/Ratings&Reviews/RatingBreakdown.jsx
@@ -1,11 +1,14 @@
 import React, {useState, useEffect, useContext} from "react"
+import Rating from 'react-rating'
 import axios from "axios"
+import AppContext from '../App/AppContext.jsx'
 import RatingsAndReviewsContext from './RatingsandReviewsContext.jsx'
 
 const RatingBreakdown = () => {
   const { product } = useContext(AppContext)
   const {product_id} = useContext(RatingsAndReviewsContext)
-  const [ratingSummary, setRatingSummary] = useState([])
+  const [ratingSummary, setRatingSummary] = useState({})
+  const averageRating = avgRating(ratingSummary)
 
   function fetchMetaData() {
     axios.get(`/api/reviews/meta/?product_id=${product_id}`)
@@ -18,11 +21,23 @@ const RatingBreakdown = () => {
     }
   }, [product])
 
+  function avgRating(rating) {
+    let totalStars = 0
+    let totalRatings = 0
+
+    for (let star in rating) {
+      totalStars += star * Number(rating[star])
+      totalRatings += Number(rating[star])
+    }
+
+    return (Math.round((totalStars / totalRatings) * 10) / 10).toString()
+  }
+
   return (
     <div>
       {/* Rating Summary */}
       <div>
-        <span></span>
+        <span>{averageRating}</span>
         <Rating />
       </div>
     </div>
