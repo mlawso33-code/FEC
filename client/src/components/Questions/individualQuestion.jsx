34aa2aad0f629465980f0edf7bbab3b121245ac0
mlawso33-code <marclawson195@gmail.com>
2021-12-30 13:54:11 -0800

    implemented helpful and report. refactored individual question props using destructure

--- client/src/components/Questions/individualQuestion.jsx
+++ client/src/components/Questions/individualQuestion.jsx
@@ -1,20 +1,50 @@
 import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+
 import AppContext from '../App/AppContext.jsx';
 
 import AnswersList from './answersList.jsx';
 
 
 const IndividualQuestion = (props) => {
+  const {question_helpfulness, question_body, question_id} = props.question
+  const [helpful, setHelpful] = useState(question_helpfulness)
+  const [helpfulClicked, setHelpfulClicked] = useState(false)
+  const [reported, setReported] = useState(false)
 
-  const helpfulNum = props.question.question_helpfulness
+  function incrementHelpful(event) {
+    event.preventDefault()
+    if (!helpfulClicked) {
+      axios.put(`/api/qa/questions/${question_id}/helpful`)
+        .then(setHelpful(helpful + 1))
+        .then(setHelpfulClicked(true))
+    }
+  }
+
+  function reportQuestion(event) {
+    event.preventDefault()
+    if(!reported) {
+      axios.put(`/api/qa/questions/${question_id}/report`)
+      .then(setReported(true))
+    }
+  }
+
+
+  //const helpfulNum = props.question.question_helpfulness
   return (
     <div style={{ maxHeight: "20vh", overflow: "scroll" }}>
       <div style={{ border: "solid" }}>
         <div style={{ display: "flex", flexDirection: "row" }}>
-          Q: {props.question.question_body}
-          <span style={{ marginLeft: "auto" }}>Helpful? Yes({helpfulNum}) | Add Answer </span>
+          Q: {question_body}
+          <div style={{ marginLeft: "auto" }}>
+            <span>Helpful?</span>
+            <a href='' style={{ color: "black" }} onClick={incrementHelpful}>Yes</a>
+            <span>({helpful})</span>
+            |
+            <a href='' style={{ color: "black" }} onClick={reportQuestion}>{reported ? 'Reported' : 'Report'}</a>
+          </div>
         </div>
-        <AnswersList question_id={props.question.questions_id} question={props.question} fetchQuestions={props.fetchQuestions} />
+        <AnswersList question_id={question_id} question={props.question} fetchQuestions={props.fetchQuestions} />
       </div>
     </div>
   )
