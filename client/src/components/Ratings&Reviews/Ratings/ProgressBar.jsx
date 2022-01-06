763b3c8da5cfa8cb7ba8f6306d5611529e1159fc
Mitchell <mitchellwgardner@gmail.com>
2022-01-06 14:35:03 -0700

    Re-colored RnR

--- client/src/components/Ratings&Reviews/Ratings/ProgressBar.jsx
+++ client/src/components/Ratings&Reviews/Ratings/ProgressBar.jsx
@@ -6,7 +6,7 @@ const ProgressBar = (props) => {
   const containerStyle = {
     display: 'inline-block',
     height: 12,
-    width: '60%',
+    width: '65%',
     backgroundColor: "#e0e0de",
     borderRadius: 50,
     marginRight: 5
