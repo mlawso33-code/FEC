9471570d9f3c726f68cb3dfddcb289240fd6b233
mlawso33-code <marclawson195@gmail.com>
2022-01-06 19:47:24 -0800

    header css

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,57 +6,16 @@ import AppContext from '../App/AppContext.jsx';
 
 const Header = (props) => {
   const [search, setSearch] = useState('')
-  // const shoes = props.product.filter((item) => {
-  //   item.name.toLowerCase().includes('shoe')
-  // })
-
-/* need to come up with a design on how we want products to be found.
-  shoes is just an example of a dropdown list for all shoe products */
-
   return (
-    <div className='header'>
-      {/* <label for="shoes">Shoes</label>
-      <select name="shoes">
-        {shoes.map((shoe =>
-          <option>
-            {shoe}
-          </option>))}
-      </select> */}
-      <form>
-        <input style={inputStyle} type="text" placeholder="Search Product" value={search}
+    <div>
+      <form className='header-search-wrapper'>
+        <input className="headerSearch" type="text" placeholder="Search Product" value={search}
           onChange={(e) => {
             setSearch(event.target.value)
           }} />
-        <button type="submit" style={buttonStyle}><i className="fas fa-search"></i></button>
+        <i className="fas fa-search"></i>
       </form>
     </div>
   )
 }
-
 export default Header
-
-// const header = {
-//   display:"block",
-//   background:"black",
-//   width:"100%",
-
-// }
-const inputStyle = {
-  display:"inline-block",
-  padding: "5px",
-  fontSize: "17px",
-  borderBottom: "4px solid grey",
-  width: "50%",
-  background: "black"
-
-}
-const buttonStyle = {
-  display:"inline-block",
-  float:"right",
-  width: "auto",
-  padding: "5px",
-  background: "black",
-  color: "white",
-  fontSize: "17px",
-  cursor: "pointer"
-}
